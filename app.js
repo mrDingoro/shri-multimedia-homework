12897744a894475c303750e608e74a4ace91c0bf
Olga Al. Popova <betta.po@yandex.ru>
2025-07-18 21:29:32 +0300

    fix video samples routing

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ app.get('/', function (_, res) {
     res.sendFile('index.html', {root: __dirname});
 });
 
+app.use('/assets', express.static('assets'));
+
+app.get('/assets/{{a}}', (_, res) => {
+    res.send(path.join(__dirname, 'assets', '{{a}}'));
+});
+
 app.use('/index.css', function (_, res) {
     res.sendFile('index.css', {root: __dirname});
 });
-
-app.use('/vids', express.static('vids'));
 
 app.listen(3000, () => {
     console.log('Приложение запущено: http://localhost:3000');
